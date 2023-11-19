16eb686852650c3d7fde2ec8e97106d8809fd3b2
Sravani020894 <sravani.mgit@gmail.com>
2023-11-19 10:33:01 -0500

    PICK CARDS from DECK

--- class27-materials/api-template/js/main.js
+++ class27-materials/api-template/js/main.js
@@ -3,15 +3,16 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const url = `https://api.nasa.gov/planetary/apod?api_key=8Hni9T2HE0jGe0DDXI6brwIQdqtUjSsHE2xAfAcw&date=${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        document.querySelector('img').src=data.url;
+        document.querySelector('h2').innerText=data.explanation;
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
-
